8c072049dbf434651ff70b9332c9bf8caa79ca44
Photorithm <photorithm@nathans-imac.local>
2016-10-20 15:15:55 -0600

    Initial Commit

---
+++ gitxcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitxcode
+//
+//  Created by Photorithm on 10/20/16.
+//  Copyright © 2016 Photorithm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
